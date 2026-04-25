e00a5d75576176b424d599cb5a3d793d78ff07fe
Anurag07-07 <adityanv8@gmail.com>
2026-04-25 15:52:17 +0530

    Refactor worker.js to use promises for file reading and improve error handling

--- JS/worker.js
+++ JS/worker.js
@@ -1,14 +1,51 @@
-import {parentPort} from 'worker_threads'
-import fs from 'fs'
+// import {parentPort} from 'worker_threads'
+// import fs from 'fs'
 
+// parentPort.on('message',(file)=>{
+//   fs.readFile(file,'utf-8',(err,data)=>{
+//     if (err) {
+//       parentPort.postMessage(err)
+//       return
+//     }
+
+//     parentPort.postMessage(data)
+//     process.exit()
+//   })
+// })
+
+import { parentPort } from 'worker_threads'
+
+import fs from 'fs'
 parentPort.on('message',(file)=>{
-  fs.readFile(file,'utf-8',(err,data)=>{
-    if (err) {
-      parentPort.postMessage(err)
+  function FileReadPromsified(file){
+    return new Promise(function(resolve,reject){
+      fs.readFile(file,'utf-8',(err,data)=>{
+        if (err) {
+          reject(`Error : ${err}`,)
+        }else{
+          resolve(data)
+        }
+      })
+    })
+  }
+
+
+  async function main() {
+    try {
+      const data = await FileReadPromsified(file)
+      let data_array = data.split(" ")
+      let f = data_array.filter((i)=>i!="")
+      //Convert Into String Again
+      let str = f.join(" ")
+      //Insert Inside File Again
+      fs.writeFileSync(file,str)
+      
+      parentPort.postMessage(str)
+      process.exit()
+    } catch (error) {
+      parentPort.postMessage(error)
       return
     }
-
-    parentPort.postMessage(data)
-    process.exit()
-  })
+  }
+  main()
 })
